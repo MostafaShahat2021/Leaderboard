2b5b7328d0fd6b7056c27d0b78a87293da7fccd1
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2023-03-07 17:38:54 +0200

    Add li Items Dynamically to UI

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
 import './style.css';
+
+const score = [
+  {
+    name: 'Mostafa',
+    playerScore: 20,
+  },
+  {
+    name: 'Adeka',
+    playerScore: 25,
+  },
+  {
+    name: 'Jeen',
+    playerScore: 25,
+  },
+  {
+    name: 'Jone',
+    playerScore: 15,
+  },
+  {
+    name: 'Doe',
+    playerScore: 40,
+  },
+];
+
+const scoreList = document.querySelector('.score-list');
+const renderScore = () => {
+  score.forEach((item) => {
+    const lielement = document.createElement('li');
+    lielement.setAttribute('class', 'score-container');
+    lielement.innerHTML = `
+      <span class="name">${item.name}: </span>
+      <span class="score">${item.playerScore}</span>
+      `;
+    scoreList.appendChild(lielement);
+  });
+};
+renderScore();
